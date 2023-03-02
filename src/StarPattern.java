4043ee0ab9f264e570e1313dd854c59a6f05ba09
RichardPark1021 <rpark1@ggc.edu>
2023-03-02 10:29:13 -0500

    All class completed

--- src/StarPattern.java
+++ src/StarPattern.java
@@ -1,9 +1,19 @@
+/**
+ * @Class: StarPattern
+ * @Author: Richard Park
+ * @Course: ITEC 2140 - 04, Spring 2023
+ * @Written: February 28, 2023
+ * Description: This program will create a pattern of stars
+ */
 public class StarPattern {
     public static void main(String[] args) {
+
         for(int i = 1; i <= 6; i++) {
             for(int j = i; j <= 6; j++){
+                //Print out the stars for each row
                 System.out.print("*");
             }
+            //skip to the next set of rows
             System.out.println();
         }
     }
